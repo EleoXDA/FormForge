e19ba2854f4f8e24c91d7061f1c9b580c93b6b6c
Elbay <elbay.malikmammadov@komre.de>
2025-12-30 21:19:48 +0100

    feat: install and configure Pinia state management

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import { Quasar, Notify, Dialog, Loading } from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 import 'quasar/src/css/index.sass'
 import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 
 app.use(Quasar, {
   plugins: {
