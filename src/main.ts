1949d91a997f5ee54ec9ed47880c435dc310587b
Elbay <elbay.malikmammadov@komre.de>
2025-12-30 21:16:35 +0100

    feat: install and configure Quasar framework

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import { Quasar, Notify, Dialog, Loading } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {
+    Notify,
+    Dialog,
+    Loading
+  }
+})
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+
+const quasarVariables = fileURLToPath(new URL('./src/styles/quasar-variables.scss', import.meta.url))
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: quasarVariables
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
