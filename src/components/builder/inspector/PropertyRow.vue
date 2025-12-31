3fa0b26987b9fd6acb7e5a49f52fdceb28aac4c9
Elbay <elbay.malikmammadov@komre.de>
2025-12-31 10:35:30 +0100

    feat: create PropertyRow label+input layout component

---
+++ src/components/builder/inspector/PropertyRow.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+interface Props {
+  label: string
+  hint?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="property-row">
+    <label class="property-row__label">
+      {{ label }}
+      <q-icon v-if="hint" name="help_outline" size="14px" class="q-ml-xs text-grey-5">
+        <q-tooltip max-width="250px">{{ hint }}</q-tooltip>
+      </q-icon>
+    </label>
+    <div class="property-row__input">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.property-row {
+  margin-bottom: 12px;
+}
+
+.property-row__label {
+  display: flex;
+  align-items: center;
+  font-size: 13px;
+  font-weight: 500;
+  color: #444;
+  margin-bottom: 4px;
+}
+
+.property-row__input {
+  width: 100%;
+}
+
+/* Make Quasar inputs more compact in the inspector */
+.property-row__input :deep(.q-field) {
+  padding-bottom: 0;
+}
+
+.property-row__input :deep(.q-field__bottom) {
+  display: none;
+}
+</style>
--- src/components/builder/inspector/index.ts
+++ src/components/builder/inspector/index.ts
@@ -1,1 +1,2 @@
 export { default as PropertySection } from './PropertySection.vue'
+export { default as PropertyRow } from './PropertyRow.vue'
